b274907a1dd3c43391c128d43b2129a94dd5a3a9
unknown <rydersiva10@gmail.com>
2019-01-29 00:13:17 +0530

    PremiumAllocationCharges calculation

--- src/pages/benifit-of-illustrations/benifit-of-illustrations.ts
+++ src/pages/benifit-of-illustrations/benifit-of-illustrations.ts
@@ -10,21 +10,28 @@ export class BenifitOfIllustrationsPage {
   payingTerm:any;
   testVal:any;
   perimum:any;
+  allocationCharges:any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.perimum = [];
+    this.allocationCharges = [];
     this.policyTerm=navParams.data.policyTerm;
     this.payingTerm=navParams.data.payingTerm;
     this.perimum = navParams.data.premiumInput;
+    this.allocationCharges = navParams.data.premiumAllocationCharges;
+
     this.testVal = [];
     console.log("policyTerm"+this.policyTerm)
     var preimumCount = this.policyTerm - this.payingTerm;
     console.log("preimumCount"+preimumCount)
     for(var p=0;p<preimumCount;p++){
        this.perimum.push(0);
+       this.allocationCharges.push(0);
     }
     console.log(this.perimum)
+    console.log(this.allocationCharges)
+
     for(var i=0;i<this.policyTerm;i++){
-      var params = {'policy':i+1,'perimum':this.perimum[i],'allocationCharges':'test','adminCharges':'test','growth':'test',
+      var params = {'policy':i+1,'perimum':this.perimum[i],'allocationCharges':this.allocationCharges[i],'adminCharges':'test','growth':'test',
                   'loyaltyAddition':'test','wealthBooster':'test','fundValue':'test','partialWithdrawal':'test', 'sumAssured':'test' }
        this.testVal.push(params);
     }
--- src/pages/life_time_classic/life_time_classic.ts
+++ src/pages/life_time_classic/life_time_classic.ts
@@ -9,109 +9,253 @@ import { BenifitOfIllustrationsPage } from '../../pages/benifit-of-illustrations
 })
 export class LifeTimeClassicPage {
 
-  premiumInput:any;
-  sumAssured:any;
-  premiumSelected:any;
-  payingTermSelected:any;
-  isReadonly:boolean=true;
-  premiumAllocationCharges:any;
-  adminCharges:any=6000;
-  wealthBooster:any;
-  policyTerm:any;
-  loyaltyAdditions:any;
-  lifeTimeClassic:any=[];
-  policyTermSelected:any;
+  premiumInput: any;
+  sumAssured: any;
+  premiumSelected: any;
+  payingTermSelected: any;
+  isReadonly: boolean = true;
+  premiumAllocationCharges: any;
+  adminCharges: any = 6000;
+  wealthBooster: any;
+  policyTerm: any;
+  loyaltyAdditions: any;
+  lifeTimeClassic: any = [];
+  policyTermSelected: any;
   constructor(public navCtrl: NavController) {
-  
+
   }
-  premiumEnter(value){
-   console.log(this.premiumInput);
-   this.calSumAssured();
-   
-   
-  }
-  premiumSelect(value){
-   // console.log(value);
-    this.premiumSelected=value;
+  premiumEnter(value) {
+    console.log(this.premiumInput);
     this.calSumAssured();
-    this.calPremiumCharges(this.premiumSelected,this.payingTermSelected)
-    
+
+
+  }
+  premiumSelect(value) {
+    // console.log(value);
+    this.premiumSelected = value;
+    this.calSumAssured();
+    this.calPremiumCharges(this.premiumSelected, this.payingTermSelected)
+
   }
 
-  calSumAssured(){
-   var sumAssured=this.premiumInput*this.premiumSelected*10;
-   if(sumAssured){
-    this.sumAssured=sumAssured;
-   }else{
-    this.sumAssured="";
-   }
+  calSumAssured() {
+    var sumAssured = this.premiumInput * this.premiumSelected * 10;
+    if (sumAssured) {
+      this.sumAssured = sumAssured;
+    } else {
+      this.sumAssured = "";
+    }
   }
 
-  payingTermSelect(value){
-    this.payingTermSelected=value;
-    this.calPremiumCharges(this.premiumSelected,this.payingTermSelected);
+  payingTermSelect(value) {
+    this.payingTermSelected = value;
+    this.calPremiumCharges(this.premiumSelected, this.payingTermSelected);
     this.calWealthBooster(this.payingTermSelected);
     this.calLoyaltyAddition(this.payingTermSelected);
-    
+
   }
 
-  calPremiumCharges(premium='',term=''){
-    if(premium=='12' ||premium=='2'){
-      if(term=='5'){
-        this.premiumAllocationCharges="1 (6%)  2 - 3 (5%)  4 (4.5%)  5 (4%)";
-      }else if(term=='7'){
-        this.premiumAllocationCharges="1 (6%)  2 - 3 (5%)  4 (4.5%)  5 (4%)  6 - 7 (4%)";
-      }else if(term=='10'){
-        this.premiumAllocationCharges="1 (6%)  2 - 3 (5%)  4 (4.5%)  5 (4%)  6 - 7 (4%)  7 > (2%)"
+  calPremiumCharges(premium = '', term = '') {
+    if (premium == '12' || premium == '2') {
+      if (term == '5') {
+        this.premiumAllocationCharges = "1 (6%)  2 - 3 (5%)  4 (4.5%)  5 (4%)";
+      } else if (term == '7') {
+        this.premiumAllocationCharges = "1 (6%)  2 - 3 (5%)  4 (4.5%)  5 (4%)  6 - 7 (4%)";
+      } else if (term == '10') {
+        this.premiumAllocationCharges = "1 (6%)  2 - 3 (5%)  4 (4.5%)  5 (4%)  6 - 7 (4%)  7 > (2%)"
       }
-    }else{
-      if(term=='5'){
-        this.premiumAllocationCharges="6%";
-      }else if(term=='7'){
-        this.premiumAllocationCharges="1 - 5 (6%)  6 - 7 (4%)";
-      }else if(term=='10'){
-        this.premiumAllocationCharges="1 - 5 (6%)  6 - 7 (4%)  7 > (2%)"
+    } else {
+      if (term == '5') {
+        this.premiumAllocationCharges = "6%";
+      } else if (term == '7') {
+        this.premiumAllocationCharges = "1 - 5 (6%)  6 - 7 (4%)";
+      } else if (term == '10') {
+        this.premiumAllocationCharges = "1 - 5 (6%)  6 - 7 (4%)  7 > (2%)"
       }
     }
   }
 
-  policyTermSelect(value){
-    this.policyTermSelected=value;
+  policyTermSelect(value) {
+    this.policyTermSelected = value;
     //this.policyTerm=value;
     //this.calWealthBooster(this.payingTermSelected,this.policyTerm);
-    
+
   }
 
-  calWealthBooster(term){
+  calWealthBooster(term) {
     console.log(term);
-    if(term=='10'){
-      this.wealthBooster=2;
-    }else{
-      this.wealthBooster=1;
+    if (term == '10') {
+      this.wealthBooster = 2;
+    } else {
+      this.wealthBooster = 1;
     }
 
   }
 
-  calLoyaltyAddition(value){
-    if(value=='5'){
-      this.loyaltyAdditions="0.10%";
-    }else{
-      this.loyaltyAdditions="6 & 7 (0.15%)  8>= (0.30%)";
+  calLoyaltyAddition(value) {
+    if (value == '5') {
+      this.loyaltyAdditions = "0.10%";
+    } else {
+      this.loyaltyAdditions = "6 & 7 (0.15%)  8>= (0.30%)";
     }
-    
+
   }
 
-  calculate(){
+  calculate() {
     console.log("calculate")
     var paying = [];
-    
-    for(var i=0;i<this.payingTermSelected;i++){
-        paying.push(this.premiumInput*this.premiumSelected);
-    }
-    console.log(paying);
+    var allocationCharges = [];
+    var payingCal = this.premiumInput * this.premiumSelected;
 
-    console.log(this.premiumInput);
-    this.lifeTimeClassic={'payingTerm':this.payingTermSelected,'policyTerm':this.policyTermSelected,'premiumInput':paying}
-    this.navCtrl.push(BenifitOfIllustrationsPage,this.lifeTimeClassic);
+    for (var i = 0; i < this.payingTermSelected; i++) {
+      paying.push(payingCal);
+      allocationCharges.push(allocationChargesCal);
+    }
+
+    if (this.premiumAllocationCharges == "6%") {
+      allocationCharges = [];
+      console.log("premiumAllocationCharges" + this.premiumAllocationCharges)
+      var allocationChargesCal = payingCal * 0.06;
+      for (var a = 0; a < this.payingTermSelected; a++) {
+        allocationCharges.push(allocationChargesCal);
+      }
+    }
+
+    if (this.premiumAllocationCharges == "1 - 5 (6%)  6 - 7 (4%)") {
+      console.log("premiumAllocationCharges" + this.premiumAllocationCharges)
+      allocationCharges = [];
+      for (var b = 0; b < this.payingTermSelected; b++) {
+        if (b < 5) {
+          var allocationChargesCal = payingCal * 0.06;
+          allocationCharges.push(allocationChargesCal);
+        }
+        else if (b >= 5) {
+          var allocationChargesCal = payingCal * 0.04;
+          allocationCharges.push(allocationChargesCal);
+
+        }
+      }
+    }
+
+    if (this.premiumAllocationCharges == "1 - 5 (6%)  6 - 7 (4%)  7 > (2%)") {
+      console.log("premiumAllocationCharges" + this.premiumAllocationCharges)
+      allocationCharges = [];
+      for (var b = 0; b < this.payingTermSelected; b++) {
+        if (b < 5) {
+          var allocationChargesCal = payingCal * 0.06;
+          allocationCharges.push(allocationChargesCal);
+        }
+        else if (b >= 5 && b < 7) {
+          var allocationChargesCal = payingCal * 0.04;
+          allocationCharges.push(allocationChargesCal);
+
+        }
+        else if (b >= 7) {
+          var allocationChargesCal = payingCal * 0.02;
+          allocationCharges.push(allocationChargesCal);
+
+        }
+
+      }
+    }
+
+    if (this.premiumAllocationCharges == "1 (6%)  2 - 3 (5%)  4 (4.5%)  5 (4%)") {
+      console.log("premiumAllocationCharges" + this.premiumAllocationCharges)
+      allocationCharges = [];
+      for (var c = 0; c < this.payingTermSelected; c++) {
+        if (c == 0) {
+          var allocationChargesCal = payingCal * 0.06;
+          allocationCharges.push(allocationChargesCal);
+        }
+        else if (c == 1 || c == 2) {
+          var allocationChargesCal = payingCal * 0.05;
+          allocationCharges.push(allocationChargesCal);
+
+        }
+        else if (c == 3) {
+          var allocationChargesCal = payingCal * 0.045;
+          allocationCharges.push(allocationChargesCal);
+
+        }
+        else if (c == 4) {
+          var allocationChargesCal = payingCal * 0.04;
+          allocationCharges.push(allocationChargesCal);
+
+        }
+
+
+      }
+    }
+
+    if (this.premiumAllocationCharges == "1 (6%)  2 - 3 (5%)  4 (4.5%)  5 (4%)  6 - 7 (4%)") {
+      console.log("premiumAllocationCharges" + this.premiumAllocationCharges)
+      allocationCharges = [];
+      for (var d = 0; d < this.payingTermSelected; d++) {
+        if (d == 0) {
+          var allocationChargesCal = payingCal * 0.06;
+          allocationCharges.push(allocationChargesCal);
+        }
+        else if (d == 1 || d == 2) {
+          var allocationChargesCal = payingCal * 0.05;
+          allocationCharges.push(allocationChargesCal);
+
+        }
+        else if (d == 3) {
+          var allocationChargesCal = payingCal * 0.045;
+          allocationCharges.push(allocationChargesCal);
+
+        }
+        else if (d > 3) {
+          var allocationChargesCal = payingCal * 0.04;
+          allocationCharges.push(allocationChargesCal);
+
+        }
+
+      }
+    }
+
+    if (this.premiumAllocationCharges == "1 (6%)  2 - 3 (5%)  4 (4.5%)  5 (4%)  6 - 7 (4%)  7 > (2%)") {
+      console.log("premiumAllocationCharges" + this.premiumAllocationCharges)
+      allocationCharges = [];
+      for (var e = 0; e < this.payingTermSelected; e++) {
+        if (e == 0) {
+          var allocationChargesCal = payingCal * 0.06;
+          allocationCharges.push(allocationChargesCal);
+        }
+        else if (e == 1 || e == 2) {
+          var allocationChargesCal = payingCal * 0.05;
+          allocationCharges.push(allocationChargesCal);
+
+        }
+        else if (e == 3) {
+          var allocationChargesCal = payingCal * 0.045;
+          allocationCharges.push(allocationChargesCal);
+
+        }
+        else if (e > 3 && e < 7) {
+          var allocationChargesCal = payingCal * 0.04;
+          allocationCharges.push(allocationChargesCal);
+
+        }
+        else if (e >= 7) {
+          var allocationChargesCal = payingCal * 0.02;
+          allocationCharges.push(allocationChargesCal);
+
+        }
+
+
+      }
+    }
+
+
+
+    console.log(paying);
+    console.log(allocationCharges);
+
+    if (allocationCharges.length > 0) {
+      this.lifeTimeClassic = { 'payingTerm': this.payingTermSelected, 'policyTerm': this.policyTermSelected, 'premiumInput': paying, "premiumAllocationCharges": allocationCharges }
+      this.navCtrl.push(BenifitOfIllustrationsPage, this.lifeTimeClassic);
+    }
+
   }
 }
